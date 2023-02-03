945f4915d51a340fc1cdadea22c3655284e5a43e
Ilia Filimonov <9991571155i@gmail.com>
2023-02-03 12:22:25 +0700

    pdfToJpeg convert examples

---
+++ src/pages/Example.tsx
@@ -0,0 +1,8 @@
+import PdfToJpegConverter from '@/components/PdfToJpegConverter/PdfToJpegConverter';
+import React from 'react';
+
+const Example: React.FC = () => (
+  <PdfToJpegConverter />
+);
+
+export default Example;
