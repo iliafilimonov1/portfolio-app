87d511262dde72e9c9c1c19e6041415173a7710b
Ilia Filimonov <9991571155i@gmail.com>
2023-02-17 21:46:17 +0700

    edit fonts, fixed button and navigation components

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,7 +3,6 @@ import { extractStyles } from '@/services/utils';
 import { useRouter } from 'next/router';
 import React from 'react';
 import Button from '../ui/Button/Button';
-// import Drawer from '../ui/Drawer/Drawer';
 
 type NavItem = { label: string; link: string; subItem?: string[] };
 
@@ -13,16 +12,14 @@ const navItems: NavItem[] = [
   { label: 'Tesseract', link: '/Tesseract' },
   { label: 'Framer-motion', link: '/FramerMotion' },
   // { label: 'RTKQuery', link: '/RTKQuery' },
-  // { label: 'NASAApi', link: '/NasaApi' },
+  { label: 'NASAApi', link: '/NasaApi' },
   // { label: 'History', link: '/History' },
 ];
 const Navigation: React.FC = () => {
   const router = useRouter();
 
-  // const [isDrawerOpen, setDrawerOpen] = useState(false);
-
   return (
-    <div className={
+    <header className={
       extractStyles`
         px-6 flex flex-wrap sticky top-0 left-0 right-0 z-[9999]
          bg-slate-100 items-center shadow-lg
@@ -30,31 +27,19 @@ const Navigation: React.FC = () => {
       `
     }
     >
-      {/* <button
-        onClick={() => setDrawerOpen(true)}
-        type="button"
-      >
-        Open drawer
-      </button>
-      <Drawer
-        isOpen={isDrawerOpen}
-        setIsOpen={(value: boolean) => setDrawerOpen(value)}
-      >
-        asdasda
-      </Drawer> */}
       {
         navItems.map((item) => (
-          <div key={item.link}>
+          <nav key={item.link}>
             <Button
-              className="text-gray-700 hover:text-gray-500 px-4 p-2 rounded-sm transition-all tracking-wider text-xl"
               onClick={() => router.push(item.link)}
+              variant="ghost"
             >
               {item.label}
             </Button>
-          </div>
+          </nav>
         ))
       }
-    </div>
+    </header>
   );
 };
 
