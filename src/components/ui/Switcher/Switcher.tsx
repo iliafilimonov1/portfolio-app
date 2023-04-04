ba7026a22fa47b2557390a6373d73ef9f1980c48
Dinar <pravovoy116@gmail.com>
2023-04-04 22:28:28 +0300

    #completed

--- src/components/ui/Switcher/Switcher.tsx
+++ src/components/ui/Switcher/Switcher.tsx
@@ -1,18 +1,19 @@
 import { extractStyles } from '@/services/utils';
 import React, { useState } from 'react';
+import useToggle from 'usehooks-ts/dist/esm/useToggle/useToggle';
 
 /** Свитчер */
 const Switcher: React.FC = () => {
-  const [active, setActive] = useState(false);
+  const [toggle, setToggle] = useToggle();
 
   return (
     <div
       className=" w-[60px] h-[25px] rounded-full overflow-hidden"
-      onClick={() => setActive(!active)}
+      onClick={setToggle}
     >
       <div className={
         extractStyles`switcher
-          ${active ? 'border-l-[60px]' : 'border-r-[60px]'}
+          ${toggle ? 'border-l-[60px]' : 'border-r-[60px]'}
         `
       }
       >
