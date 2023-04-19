9ea97472e44072083ddd265ab6c27fb095f6f07a
Dinar Sharipov <Donotcoverr@mail.ru>
2023-04-19 22:21:33 +0300

    process

--- src/components/Tab/TabLayout.tsx
+++ src/components/Tab/TabLayout.tsx
@@ -1,11 +1,41 @@
-import React, { useMemo, useState } from 'react';
-import { TabLayoutProps } from './types';
+import React, { useMemo, useState } from "react";
+import { TabLayoutProps } from "./types";
+import { extractStyles } from "@/services/utils";
 
-const TabLayout: React.FC<TabLayoutProps> = ({children})=> {
-  const childrenArray = useMemo(()=> {
-    return Array.isArray(children) ? children.flat(): [children]
-  },[])
-  const [activeTab, setActiveTab] = useState()
-  return (<div className='flex'>{children}</div>)};
+const TabLayout: React.FC<TabLayoutProps> = ({ children }) => {
+  const childrenArray = useMemo(() => {
+    return Array.isArray(children) ? children.flat() : [children];
+  }, []);
+  const [activeTabAccessor, setActiveTabAccessor] = useState<
+    string | undefined
+  >();
+
+  const activeContent = useMemo(() => {
+    return childrenArray.find(
+      (child) => child?.props.accessor === activeTabAccessor
+    );
+  }, []);
+
+  return (
+    <div className="flex">
+      <div>
+        {childrenArray.map((child) => (
+          <div
+            className={extractStyles`
+            ${
+              child?.props.accessor === activeTabAccessor
+                ? "bg-red-400"
+                : "bg-green-400"
+            }
+          `}
+          >
+            {child?.props.title}
+          </div>
+        ))}
+      </div>
+      <div>{activeContent?.props.children}</div>
+    </div>
+  );
+};
 
 export default React.memo(TabLayout);
