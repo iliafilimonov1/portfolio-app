930640da3ac05fc8afd9351342d62b8d51793bc9
Dinar <pravovoy116@gmail.com>
2023-04-23 20:37:36 +0300

    process

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    "project": "./tsconfig.json",
     ecmaFeatures: {
       jsx: true,
     },
@@ -29,51 +30,39 @@ module.exports = {
     'react',
     '@typescript-eslint',
     'import',
-    'import-newlines',
-    'react-hooks',
   ],
   rules: {
     'no-unused-vars': 'off',
-    '@typescript-eslint/consistent-type-imports': 'warn',
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        caughtErrorsIgnorePattern: '^_',
-      },
+    '@typescript-eslint/no-unused-vars': ['error'
     ],
+
     'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': ['error'],
+    '@typescript-eslint/no-use-before-define': ['error'
+    ],
 
     'no-undef': 'off',
     'react/jsx-no-undef': 'error',
 
     'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': ['error'],
+    '@typescript-eslint/no-shadow': ['error'
+    ],
 
     indent: 'off',
-    '@typescript-eslint/indent': ['error', 2, { SwitchCase: 1 }],
-    '@typescript-eslint/type-annotation-spacing': 'error',
+    '@typescript-eslint/indent': ['error',
+      2,
+      { SwitchCase: 1
+      }
+    ],
 
     'brace-style': 'off',
-    '@typescript-eslint/brace-style': ['error'],
+    '@typescript-eslint/brace-style': ['error'
+    ],
 
     '@typescript-eslint/no-explicit-any': 'error',
     'no-restricted-syntax': 1,
-    'spaced-comment': ['error', 'always'],
-    'no-underscore-dangle': 'off',
-    'padding-line-between-statements': [
-      'error',
-      { blankLine: 'always', prev: 'const', next: 'return' },
-      { blankLine: 'always', prev: 'import', next: '*' },
-      { blankLine: 'never', prev: 'import', next: 'import' },
-      { blankLine: 'always', prev: '*', next: 'export' },
-      { blankLine: 'any', prev: 'export', next: 'export' },
+    'spaced-comment': ['error', 'always'
     ],
-
-    'class-methods-use-this': 'off',
+    'no-underscore-dangle': 'off',
 
     'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
@@ -81,41 +70,62 @@ module.exports = {
     'import/extensions': 'off',
     'import/prefer-default-export': 'off',
 
-    'import-newlines/enforce': [
-      'error',
-      {
-        items: 3,
-        'max-len': 150,
-        semi: false,
-      },
+    'react-hooks/exhaustive-deps': 'off',
+
+    'react/destructuring-assignment': ['error', 'always'
     ],
-
-    'react-hooks/exhaustive-deps': 'warn',
-
-    'react/destructuring-assignment': ['error', 'always'],
-    'react/function-component-definition': ['error', {
+    'react/function-component-definition': ['error',
+      {
       namedComponents: 'arrow-function',
       unnamedComponents: 'arrow-function',
-    }],
+      }
+    ],
     'react/no-array-index-key': 'error',
     'react/jsx-closing-bracket-location': 'error',
     'react/jsx-closing-tag-location': 'error',
-    'react/jsx-first-prop-new-line': ['error', 'multiline-multiprop'],
-    'react/jsx-indent': [2, 2, { indentLogicalExpressions: true }],
-    'react/jsx-indent-props': [2, 2],
-    'react/jsx-max-props-per-line': [1, { when: 'always' }],
-    'react/jsx-one-expression-per-line': ['error', { allow: 'single-child' }],
-    'react/jsx-pascal-case': ['error', { allowNamespace: true }],
+    'react/jsx-first-prop-new-line': ['error', 'multiline-multiprop'
+    ],
+    'react/jsx-indent': [
+      2,
+      2,
+      { indentLogicalExpressions: true
+      }
+    ],
+    'react/jsx-indent-props': [
+      2,
+      2
+    ],
+    'react/jsx-max-props-per-line': [
+      1,
+      { when: 'always'
+      }
+    ],
+    'react/jsx-one-expression-per-line': ['error',
+      { allow: 'single-child'
+      }
+    ],
+    'react/jsx-pascal-case': ['error',
+      { allowNamespace: true
+      }
+    ],
     'react/jsx-props-no-multi-spaces': 'error',
-    'react/jsx-sort-props': ['warn', { shorthandLast: true, reservedFirst: true }],
-    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
+    'react/jsx-sort-props': ['warn',
+      { shorthandLast: true, reservedFirst: true
+      }
+    ],
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.jsx', '.tsx'
+        ]
+      }
+    ],
     'react/require-default-props': 0,
     'react/jsx-props-no-spreading': 'off',
-    'react/jsx-no-useless-fragment': [2, { allowExpressions: true }],
-    curly: ['error', 'all'],
+    curly: ['error', 'all'
+    ],
     'no-plusplus': 'off',
-    '@typescript-eslint/member-delimiter-style': ['error'],
-    'max-len': ['error', { code: 150 }],
-    'no-param-reassign': 'off',
+    '@typescript-eslint/member-delimiter-style': ['error'
+    ],
+    'no-param-reassign': ["error", { "props": true, "ignorePropertyModificationsFor": ["state"] }]
   },
 };
--- src/components/Tab/TabPanel.tsx
+++ src/components/Tab/TabPanel.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { TabProps } from './types';
 
-const TabPanel: React.FC<TabProps> = ({children, title})=> <div>
+const TabPanel: React.FC<TabProps> = ({ children, title }) => (
+  <div>
     <div>{title}</div>
     <div>{children}</div>
-</div>
+  </div>
+);
 
 export default React.memo(TabPanel);
