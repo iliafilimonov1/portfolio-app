2ea158d8137e09fb303c5982e9f1db732dc04c32
Dinar <pravovoy116@gmail.com>
2023-06-06 23:35:04 +0300

    delete dropdown

--- src/components/ui/Dropdown/types.tsx
+++
@@ -1,8 +0,0 @@
-import { ButtonProps } from '../Button/Button';
-
-export type DropdownButton = ButtonProps & { id: string; label: string };
-
-export interface DropdownProps {
-  buttons: DropdownButton[];
-  defaultButton?: DropdownButton;
-}
