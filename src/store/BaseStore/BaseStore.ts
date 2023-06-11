c48fe30addc6d8905aadd8dfd6a2da0434807438
Dinar <pravovoy116@gmail.com>
2023-06-11 21:40:04 +0300

    process

---
+++ src/store/BaseStore/BaseStore.ts
@@ -0,0 +1,34 @@
+import { makeAutoObservable, runInAction } from 'mobx';
+import StateStore from '../StateStores/StateStore';
+import { Status } from '../StateStores/types';
+
+/** Абстрактный базовый класс с состоянием */
+abstract class BaseStore {
+  public _state?: StateStore;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  /**
+   * Функция обертка в которую необходимо передавать асинхронную функцию
+   * чтобы контролировать состояние загрузки
+   */
+  protected async runWithStateControl<T = void>(func: () => (Promise<T>)): Promise<T | undefined> {
+    this._state = new StateStore(Status.Fetching);
+    try {
+      const result = await func();
+      runInAction(() => {
+        this._state = new StateStore(Status.Success);
+      });
+      return result;
+    } catch (e) {
+      runInAction(() => {
+        this._state = new StateStore(Status.Error);
+      });
+      return undefined;
+    }
+  }
+}
+
+export default BaseStore;
