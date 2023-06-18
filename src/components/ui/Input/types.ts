74a1fb52f2132b4bcec5303f7ea6557cf159a53c
Ilia Filimonov <9991571155i@gmail.com>
2023-06-18 23:31:22 +0400

    select component

--- src/components/ui/Input/types.ts
+++ src/components/ui/Input/types.ts
@@ -2,10 +2,12 @@ export interface InputProps {
   id?: string;
   label?: string;
   value?: string;
+  className?: string;
   onChange?: (value: string) => void;
   onBlur?: () => void;
   placeholder?: string;
   onClick?: () => void;
   error?: string;
   postfix?: React.ReactNode;
+  onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
 }
